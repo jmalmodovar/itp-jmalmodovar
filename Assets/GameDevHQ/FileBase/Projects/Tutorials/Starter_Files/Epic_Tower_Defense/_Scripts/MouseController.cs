0dabb7382b3cef7359fbbd4e4a42d5dbf02f0738
juanmanolo <juanmanolo@gmail.com>
2020-06-10 23:02:40 +0200

    Week 2. Camera controller, movement and zoom implemented.

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/_Scripts/MouseController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 5f;
+    [SerializeField]
+    private float _zoomSpeed = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+        float screenMarginWidth = screenWidth * 0.2f;
+        float screenMarginHeight = screenHeight * 0.2f;
+
+
+        if (Input.mousePosition.x < screenMarginWidth)
+        {
+            transform.Translate(Vector3.left * _moveSpeed * Time.deltaTime);
+        }
+        else if(Input.mousePosition.x > screenWidth - screenMarginWidth)
+        {
+            transform.Translate(Vector3.right * _moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.mousePosition.y < screenMarginHeight)
+        {
+            transform.Translate(Vector3.down * _moveSpeed * Time.deltaTime);
+        }
+        else if (Input.mousePosition.y > screenHeight - screenMarginHeight)
+        {
+            transform.Translate(Vector3.up * _moveSpeed * Time.deltaTime);
+        }
+
+        transform.Translate(Vector3.forward * Input.GetAxis("Mouse ScrollWheel") * _zoomSpeed);
+
+    }
+}
