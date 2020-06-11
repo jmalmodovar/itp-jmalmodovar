7b65fc2ca5bb9ab7caffa28bf29effd63123164c
juanmanolo <juanmanolo@gmail.com>
2020-06-11 21:58:37 +0200

    Week 2. Camera controller, WASD keys implemented.

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/_Scripts/CameraController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 10f;
+    [SerializeField]
+    private float _zoomSpeed = 10f;
+    [SerializeField]
+    private float _marginPercentage = 0.15f;
+
+    void Update()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+        float screenMarginWidth = screenWidth * _marginPercentage;
+        float screenMarginHeight = screenHeight * _marginPercentage;
+
+
+        //Keyboard WASD
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * _moveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * _moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * _moveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * _moveSpeed * Time.deltaTime);
+        }
+
+        // Mouse Margins
+        if (Input.mousePosition.x < screenMarginWidth)
+        {
+            transform.Translate(Vector3.left * _moveSpeed * Time.deltaTime);
+        }
+        else if (Input.mousePosition.x > screenWidth - screenMarginWidth)
+        {
+            transform.Translate(Vector3.right * _moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.mousePosition.y < screenMarginHeight)
+        {
+            transform.Translate(Vector3.down * _moveSpeed * Time.deltaTime);
+        }
+        else if (Input.mousePosition.y > screenHeight - screenMarginHeight)
+        {
+            transform.Translate(Vector3.up * _moveSpeed * Time.deltaTime);
+        }
+
+        // Mouse Zoom
+        transform.Translate(Vector3.forward * Input.GetAxis("Mouse ScrollWheel") * _zoomSpeed);
+    }
+}
